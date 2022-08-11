7b98156a9b9473f1da4d0d53d778e28ffeb91454
luca-rigutti <101995676+luca-rigutti@users.noreply.github.com>
2022-08-11 20:57:41 +0000

    Deploying to gh-pages from @ luca-rigutti/todo-nextjs@378ac8e0fc340978663e76f335b093b485004b0f 🚀

---
+++ _next/static/5Zw0J1xf1VFN6WJ-I0N4A/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-fe25a515f0a71fd1.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
