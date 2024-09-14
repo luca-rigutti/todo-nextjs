1f4d2796be45be5b3a6aa9418c691bba3d1ffc85
Luca Rigutti <info@lucarigutti.it>
2024-09-14 11:34:52 +0200

    feat(index): Save the content in localStorage
    Why?
    This is to avoid to lost all when the page is refresh
    
    Related of #31

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,30 @@ import styles from '../styles/Home.module.css'
 import Todoadd from '../comps/todo-add'
 import TodoList from '../comps/todo-list'
 import Navbar from '../comps/navbar'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function Home() {
 
   const [list, setList] = useState([]);
+  const [isListLoaded, setIsListLoaded] = useState(false);
+
+  // Load list from localStorage on client-side after component mounts
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const savedList = window.localStorage.getItem('list');
+      if (savedList) {
+        setList(JSON.parse(savedList));
+      }
+      setIsListLoaded(true);
+    }
+  }, []);
+
+  // Update localStorage whenever 'list' changes
+  useEffect(() => {
+      if (isListLoaded && typeof window !== 'undefined') {
+        window.localStorage.setItem('list', JSON.stringify(list));
+      }
+    }, [list]);
 
   const addToList = (childdata) => {
     setList([...list,childdata])
