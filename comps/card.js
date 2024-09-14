82446e6b1ebd159d699c7ddf3ea83ae8acb25ac0
Luca Rigutti <info@lucarigutti.it>
2024-09-14 11:38:48 +0200

    feat(card): Send event to index page to performe delete object
    Related of #31

--- comps/card.js
+++ comps/card.js
@@ -1,11 +1,28 @@
-function Card({title,description}){
+import { useContext } from 'react';
+import { EventContext } from '../pages/index'; // Import the context
+
+function Card({title,id,description}){
+    const handleCardEvent = useContext(EventContext);
+
+  const handleClick = () => {
+    handleCardEvent(id); // Send the id back to Home component
+  };
     return (
-        <div className="card mb-10">
-            <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text">{description}</p>
+        <div className="row g-0">
+            <div className="col-md-10">
+                <div className="card mb-10">
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
+                </div>
+            </div>
+            </div>
+            <div className="col-md-2">
+                {id}
+                <button onClick={handleClick}>Delete</button>
             </div>
         </div>
+        
     )
 }
 
